3a6cf48ed462db5806d53d0473fa5f4cea85ad6e
William Bailkoski <wab199@student.bham.ac.uk>
2024-07-18 19:11:17 +0100

    refactored neural and model simulation. need to implement cegis and then re-verify

---
+++ code/transition_kernel.py
@@ -0,0 +1,30 @@
+import numpy as np
+from itertools import product
+
+
+# Define the failure function phi
+def phi(v_vbar):
+    indicator = []
+    for i in range(0, len(v_vbar)):
+        if v_vbar[i][0] < 0:
+            indicator.append([1])
+        else:
+            indicator.append([0])
+    return indicator
+
+
+# Stochasticity
+def state_noise(state):
+    noise_dict = {}
+    for k in range(len(state)):
+        noise_dict["agent_" + str(k)] = (state[k] * 0.9, state[k] * 1.1)
+
+    return list(product(*noise_dict.values()))
+
+
+def transition_kernel(previous_state, C, B, r):
+    # returns old state and uniform list of possible states
+    Cx = np.dot(C, previous_state)
+    Bphi = np.dot(B, phi(previous_state))
+
+    return state_noise(Cx + r - Bphi)  # add noise
